7b7564355f82ba93db73002995b32f16b8774ff9
JeffRisberg <j.s.risberg@gmail.com>
2021-03-17 23:55:55 -0700

    created example of posting a json object

--- src/Main.jl
+++ src/Main.jl
@@ -6,24 +6,35 @@ Main:
 =#
 using Genie, Genie.Router, Genie.Requests, Genie.Renderer.Json
 
-struct Book
-  id::Int
+import Base: ==
+
+using StructTypes
+
+mutable struct Book
+  id::Int64
   title::String
   author::String
-  pages::Int
+  pages::Int64
 end
 
-book1 = Book(1, "Harry Potter", "J. K. Rowling", 423)
-book2 = Book(2, "Tom Swift", "Victor Appleton II", 216)
+
+==(x::Book, y::Book) = x.id == y.id
+Book() = Book(0, "", "", 0)
+Bool(title, author, pages) = Book(0, title, author, pages)
+StructTypes.StructType(::Type{Book}) = StructTypes.Mutable()
+
+book1 = Book(1, "Harry Potter and the Sorcers Stone", "J. K. Rowling", 423)
+book2 = Book(2, "Tom Swift and his Flying Lab", "Victor Appleton II", 216)
+book3 = Book(3, "Tom Swift and his Jetmarine", "Victor Appleton II", 221)
+
 
 books = Book[]
-append!(books, [book1])
-append!(books, [book2])
+append!(books, [book1, book2])
+append!(books, [book3])
 
+Base.convert(::Type{Int64}, v::SubString{String}) = parse(Int64, v)
 
-Base.convert(::Type{Int}, v::SubString{String}) = parse(Int, v)
-
-route("/books/:id::Int", method = GET, named = :withParam) do
+route("/books/:id::Int64", method = GET, named = :withParam) do
   id = payload(:id)
   for book in books
     if book.id == id
@@ -33,8 +44,23 @@ route("/books/:id::Int", method = GET, named = :withParam) do
   json([])
 end
 
-route("/books", method = GET, named = :noParam) do
+route("/books", method = GET, named = :booksNoParam) do
   json(books)
+end
+
+route("/books", method = POST) do
+  @show jsonpayload()
+  @show rawpayload()
+
+  title = jsonpayload()["title"]
+  author = jsonpayload()["author"]
+  pages = jsonpayload()["pages"]
+
+  nextId = size(books, 1) + 1
+  book = Book(nextId, title, author, pages)
+  append!(books, [book])
+
+  json("New Book $(nextId) $(title) $(author) $(pages)")
 end
 
 up(8000, async = false)
